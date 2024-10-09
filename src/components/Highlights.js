b3b5eba82dd59eab422bd37235c33a402db5abd6
michele di stadio <distadiomichele93@gmail.com>
2024-10-09 14:44:41 +0200

    Highlights

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Highlights.css';
-
+import Gsalad from './images/greek salad.jpg';
+import Bruschetta from './images/bruschetta.png';
+import Ldes from './images/lemon dessert.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTruck } from '@fortawesome/free-solid-svg-icons';
 
 const Highlights = () => {
   return (
@@ -11,26 +15,65 @@ const Highlights = () => {
           <a href="#menu">Online Menu</a>
         </div>
         <div className="highlights-grid">
-          <div className="highlight">
-            <img src="https://example.com/image1.jpg" alt="highlight-image" />
-            <h3>Spicy Chicken Wings</h3>
-            <p>$12.99</p>
+          <div className="card-highlight">
+            <img src={Gsalad} alt="Greek Salad" />
+            <div className="card-title-price">
+              <h3>Greek Salad</h3>
+              <p>$12.99</p>
+            </div>
+            <div className="card-description">
+              <p>
+                The famous greek salad of crispy
+                lettuce, peppers, olives and
+                our Chicago style feta cheese garnished with
+                crunchy garlic and rosemary
+                croutons.
+              </p>
+            </div>
+            <div className='order-now'>
+              <a href='#order-online'>Order now <FontAwesomeIcon icon={faTruck} /></a>
+            </div>
           </div>
-          <div className="highlight">
-            <img src="https://example.com/image2.jpg" alt="highlight-image" />
-            <h3>Tenderloin Salad</h3>
-            <p>$9.99</p>
+
+          <div className="card-highlight">
+            <img src={Bruschetta} alt="Bruschetta" />
+            <div className="card-title-price">
+              <h3>Bruschetta</h3>
+              <p>$5.99</p>
+            </div>
+            <div className="card-description">
+              <p>
+                Our Bruschetta is made from grilled
+                bread that has been smeared with salt and olive oil
+              </p>
+            </div>
+            <div className='order-now'>
+              <a href='#order-online'>Order now <FontAwesomeIcon icon={faTruck} /></a>
+            </div>
           </div>
-          <div className="highlight">
-            <img src="https://example.com/image3.jpg" alt="highlight-image" />
-            <h3>Grilled Chicken Breast</h3>
-            <p>$15.99</p>
+
+          <div className="card-highlight">
+            <img src={Ldes} alt="Lemon dessert" />
+            <div className="card-title-price">
+              <h3>Lemon Dessert</h3>
+              <p>$5.00</p>
+            </div>
+            <div className="card-description">
+              <p>
+                This comes straight from
+                grandma's recipe book, every
+                last ingriedient has been sourced and is as authentic as can be
+                imagined.
+              </p>
+            </div>
+            <div className='order-now'>
+              <a href='#order-online'>Order now <FontAwesomeIcon icon={faTruck} /></a>
+            </div>
           </div>
         </div>
       </div>
     </section>
   )
 }
-
 
 export default Highlights;
